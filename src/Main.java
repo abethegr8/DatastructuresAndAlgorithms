9123bbc94f32b949ee936058fde043b7f028439c
Abraham Martinez <martinez.abraham@gmail.com>
2022-06-29 18:25:08 -0700

    creating new pull request

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 20;
+        int n = 4;
         System.out.println("The fib sequence of: " + n + " is: " + Fibonnaci.fibBruteForce(n));
 
-        int[] arr1 = {2,3,2,3,4,4,5,5,1};
-        System.out.println("Single number: " + SingleNumber.singleNumberOptimized(arr1));
+        //int[] arr1 = {2,3,2,3,4,4,5,5,1};
+        //System.out.println("Single number: " + SingleNumber.singleNumberOptimized(arr1));
+
+        //making pull request
     }
 }
---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,14 @@
+/*Week 4 Hackerrank Question
+Given a linked list, swap every two adjacent nodes and return its head.
+Example:
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+* Complete the 'swapNodesInPairs' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
+
+ */
+
+public class SwapNodesInPairs {
+
+}
